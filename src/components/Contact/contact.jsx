592d7bb4d9d7b447e65b33dc5f326c24746dcf6c
Simon Milord <simon.milord@gmail.com>
2022-07-17 16:16:41 -0400

    added contact section

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./contact.scss";
-
+import PaintBrush from "../../assets/Svgs/paintbrush.svg";
 
 export default function Contact(props) {
 
@@ -8,8 +8,9 @@ export default function Contact(props) {
     <div className='contact'>
       <h2 className='contact__title'>Let's chat</h2>
       <p className='contact__content'>Still curious? Reach out to me at {" "}
-      <a href='mailto:sadhika.billa@gmail.com' className='contact__email' style={{color: "#AD3862"}}>sadhika.billa@gmail.com</a>
+        <a href='mailto:sadhika.billa@gmail.com' className='contact__email' style={{color: "#AD3862"}}>sadhika.billa@gmail.com</a>
       </p>
+      <img src={PaintBrush} alt="paint brush and paint" className='contact__img'></img>
     </div>
   );
 }
